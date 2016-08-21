ded6677e7192224915659a1564c8752dfac8cc37
ameenkhaan07 <ameenkhan07@gmail.com>
2016-08-21 20:59:43 +0530

    concrete val/var fields in abstract class

--- recipes/src/main/scala/Pet.scala
+++ recipes/src/main/scala/Pet.scala
@@ -5,22 +5,27 @@ package ameenkhan07.recipes
 // used to define properites, fields could be abstract or have concrete definitions
 // ABSTRACT vs TRAITS
 
+// Abstract Classes:
+// val and var fields need to be redeclared in conccrete Classes
+// Concrete val fields in in abstract classes
 
 //ABSTRACT CLASS
 abstract class Pet (name: String) {
-	val greeting: String
+	val greeting: String="Hello"
 	var age: Int
 	def sayHello() = { 
 		println(greeting) 
 	}
+	def run 
 	override def toString = s"I say $greeting, and I'm $age"
 }
 
 
 //CONCRETE CLASSES
 class Dog2 (name: String) extends Pet (name) {
-	val greeting = "Woof"
+	override val greeting = "Woof"
 	var age = 2
+	def run { println("Dog is running") }
 }
 
 class Cat2 (name: String) extends Pet (name) {
